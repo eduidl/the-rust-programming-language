f645bf381d1503b3521bd1b5d5b5a5a72e24f960
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2018-05-14 19:05:45 +0900

    10. Generic Types, Traits, and Lifetimes

---
+++ 10_Generic_Types_Traits_and_Lifetimes/generic_types/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+    let p = Point { x: 3.0, y: 4.0 };
+    println!("distance from origin to p is {}", p.distance_from_origin());
+    
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    } 
+}

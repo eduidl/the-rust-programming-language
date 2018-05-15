609444948055277e745042b233d277ac7ab02dbf
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2018-05-15 15:32:08 +0900

    16. Fealess Concurrency

---
+++ 16_Fearless_Concurrency/threads/src/main.rs
@@ -0,0 +1,50 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi numebr {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    
+    handle.join().unwrap();
+    
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector {:?}", v);
+    });
+
+    handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+
+            // 所有権を持たないためコンパイルエラー
+            // println!("val is {}", val);
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
